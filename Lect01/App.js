a6c8e9a6f8e1f22bdb43a42df07daa54f9040372
Ashish Singh <ashishsingh@Ashishs-MacBook-Air.local>
2024-09-08 07:12:13 +0530

    02_lect

--- Lect01/App.js
+++
@@ -1,6 +0,0 @@
-const heading=React.createElement("h1",{},"hi Ashish is here for all!") 
-    console.log(heading)
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading)  
-    // React, rendering refers to the process of displaying a component's output (UI) based on the current state and props. The component's render method or return statement determines what gets displayed on the screen.
-    
